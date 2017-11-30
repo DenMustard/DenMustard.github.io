e94e2c74e1f4d4ba4ddf5daf0d9ad6f4ac5f3a1d
DenMustard <32615739+DenMustard@users.noreply.github.com>
2017-11-30 02:15:21 +0100

    blur

--- js/index.js
+++ js/index.js
@@ -8,6 +8,20 @@ $(document).ready(function(){
   $('.close-contact').on('click', function(){
     $('.popup-call-contact').toggleClass('popup-call-contact-show');
   });
+  
+});
+
+$(document).ready(function(){
+  $('.click-popup-call-contact').on('click', function(){
+    $('.blur').toggleClass('blur-blur');
+  });
+});
+
+$(document).ready(function(){
+  $('.close-contact').on('click', function(){
+    $('.blur').toggleClass('blur-blur');
+  });
+  
 });
 
 function validate(evt) {
